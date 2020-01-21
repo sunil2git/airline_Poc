a82084a83dfdd5e085538bbbdf125a08e381affc
ACS <acs@ACSs-MacBook-Pro-2.local>
2020-01-21 14:39:51 +0530

    data load on respective dataframes

--- src/main/scala/poc/airLinePocData.scala
+++ src/main/scala/poc/airLinePocData.scala
@@ -1,4 +1,5 @@
 package poc
+
 import org.apache.spark.sql.expressions.Window
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
@@ -14,44 +15,21 @@ object airLinePocData {
     val spark = SparkSession
       .builder()
       .master("local[*]")
-      .appName("ColumnWork")
+      .appName("Airline-POC")
       .getOrCreate()
     import spark.implicits._
 
-    // println(" test scala program")
+    val airlineData = spark.read.csv("/Users/acs/Documents/sparkData/airlinePocRepo/airline_Poc/src/Docs&Data/airline_data")
+      .toDF("id", "airlineName", "alias", "IATA", "ICAO", "Callsign", "country", "active")
 
-    case class Song(title:String,artist:String,track:Int)
-    val stay=Song("Stay","Inna",4)
-    //  println(s"Hello, 2+3 is ${2+3}")
+    val airportData = spark.read.csv("/Users/acs/Documents/sparkData/airlinePocRepo/airline_Poc/src/Docs&Data/airport_data")
+      .toDF("id", "airportName", "city", "country", "IATA/FAA", "ICAO", "latitude", "longitde", "Altitude", "Timezone", "DST", "place")
 
-    val df = Seq(
-      (1, "2014/01/01 23:00:01"),
-      (1, "2014/11/31 12:40:32"),
-      (1, "2016/12/29 09:54:00"),
-      (1, "2016/05/09 10:12:43")).toDF("id", "date")
-
-    // df.show()
+    val route = spark.read.csv("/Users/acs/Documents/sparkData/airlinePocRepo/airline_Poc/src/Docs&Data/route_data")
+      .toDF("airlineName", "airlineId", "source_airport", "source_airport_id", "destination_airport", "destination_airport_id", "CodeShare","stops", "unknown")
 
 
-
-
-    def sum(a:Int)(b:Int)(c:Int)={
-      a+b+c}
-    val sumValue= sum(3)(2)(1)
-    // println(sumValue)
-
-    val data = Seq(
-      (1, "a","jan","10"),
-      (1, "b","jan","10"),
-      (2, "c","jan","10"),
-      (2, "a","jan","5")).toDF("id", "physician","month","value")
-
-    data.createTempView("test")
-
-    spark.sql("select t1.*, t1.value/t2.value from test t1 join test t2 on t1.physician=t2.physician where t1.id != t2.id").show
+    route.show()
 
   }
-
-
-
 }
