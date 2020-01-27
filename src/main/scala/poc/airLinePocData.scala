85056711c42be69f4c733c5377b87831942e498a
ACS <acs@ACSs-MacBook-Pro-2.local>
2020-01-27 19:18:24 +0530

    find top route analytics added

--- src/main/scala/poc/airLinePocData.scala
+++ src/main/scala/poc/airLinePocData.scala
@@ -1,8 +1,12 @@
 package poc
 
+import poc.utils.columnNames
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import java.time.{LocalDateTime, ZoneId, ZonedDateTime}
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
 
 object airLinePocData {
   def main(args: Array[String]): Unit = {
@@ -17,28 +21,66 @@ object airLinePocData {
       .getOrCreate()
 
     val airlineData = spark.read.csv("/Users/acs/Documents/sparkData/airlinePocRepo/airline_Poc/src/Docs&Data/airline_data")
-      .toDF("id", "airlineName", "alias", "IATA", "ICAO", "Callsign", "country", "active")
+      .toDF(columnNames.AIRLINE_ID, columnNames.AIRLINE_NAME,columnNames.ALIAS, columnNames.IATA,columnNames.ICAO, columnNames.CALLSIGN,columnNames.COUNTRY,columnNames.ACTIVE)
 
     val airportData = spark.read.csv("/Users/acs/Documents/sparkData/airlinePocRepo/airline_Poc/src/Docs&Data/airport_data")
-      .toDF("id", "airportName", "city", "country", "IATA/FAA", "ICAO", "latitude", "longitde", "Altitude", "Timezone", "DST", "place")
+      .toDF("id", "airportName", "city", "country", "IATA/FAA", "ICAO", "latitude", "longitude", "Altitude", "Timezone", "DST", "place")
 
     val route = spark.read.csv("/Users/acs/Documents/sparkData/airlinePocRepo/airline_Poc/src/Docs&Data/route_data")
-      .toDF("airlineName", "airlineId", "source_airport", "source_airport_id", "destination_airport", "destination_airport_id", "CodeShare", "stops", "unknown")
+      .toDF(columnNames.AIRLINE_NAME, columnNames.AIRLINE_ID, columnNames.SOURCE_AIRPORT, columnNames.SOURCE_AIRPORT_ID, columnNames.DESTINATION_AIRPORT, columnNames.DESTINATION_AIRPORT_ID, columnNames.CODESHARE, columnNames.STOPS,columnNames.UNKNOWN)
 
 
     val result = oddEvenCheck(3)
-
     //data.groupBy(columnNames.Winner,columnNames.Stadiuam).agg(count("*").alias("cnt")).orderBy(col("cnt").desc).show()
     val source_airportDF = route.groupBy("source_airport").agg(count("*").alias("count")).orderBy(col("count").desc)
     val destination_airportDF = route.groupBy("destination_airport").agg(count("*").alias("count")).orderBy(col("count").desc)
 
     val totalcount = source_airportDF.unionAll(destination_airportDF)
 
-    val totalDF=totalcount.groupBy("source_airport").agg(sum("count").alias("totalcount")).orderBy(col("totalcount").desc).limit(5)
+    val totalDF = totalcount.groupBy("source_airport").agg(sum("count").alias("totalcount")).orderBy(col("totalcount").desc).limit(10)
 
-    totalDF.select("source_airport").show(truncate = false)
+    //  totalDF.select("source_airport").show(truncate = false)
+    /** Top 10 busy route */
+    airportData.join(totalDF, col("IATA/FAA") === col("source_airport")).select("airportName", "city", "country", "IATA/FAA", "totalcount")
+      .orderBy(col("totalcount").desc)
+      //.show(truncate = false)
 
-    airportData.join(totalDF, col("IATA/FAA") === col("source_airport")).select("airportName","city","country","IATA/FAA","totalcount").show()
+    /** Top 10 busy airline on top 10 busy route */
+
+
+    airlineData.groupBy(columnNames.AIRLINE_NAME).agg(count("*").alias("cnt"))
+      .orderBy(col("cnt").desc).limit(5)
+
+    route.groupBy(columnNames.SOURCE_AIRPORT).agg(count("*").alias("cnt"))
+      .orderBy(col("cnt").desc).limit(5)
+
+    route.groupBy(columnNames.AIRLINE_NAME,columnNames.SOURCE_AIRPORT,columnNames.DESTINATION_AIRPORT).agg(count("*").alias("cnt"))
+      .orderBy(col("cnt").desc).limit(5)
+
+    route.groupBy(columnNames.AIRLINE_ID,columnNames.AIRLINE_NAME,columnNames.DESTINATION_AIRPORT,columnNames.SOURCE_AIRPORT).agg(count("*").alias("cnt"))
+      .orderBy(col("cnt").desc).limit(5)
+
+    import spark.implicits._
+
+    val testrouteDF=Seq(("A","pune","goa"),("B","pune","goa"),("B","goa","pune"),("C","pune","mumbai"),("A","mumbai","pune"),("A","goa","pune"),("B","mumbai","pune")
+      ,("D","mumbai","goa"),("B","goa","mumbai"))
+      .toDF("person","place1","place2")
+
+    testrouteDF.createTempView("temp")
+
+      spark.sql("select * from temp t1 join temp t2 on t1.place1 =t2.place2 where t1.place2 =t2.place1")
+
+    spark.sql("select place1,place2,count(*) from temp group by place1,place2 ")
+
+    val test=testrouteDF.groupBy(col("place1"),col("place2")).agg(count("*").alias("cnt"))
+
+    test.show()
+
+    test.createTempView("test")
+
+    val test1= spark.sql("select Distinct(*),case when t1.place1= t2.place2 and t2.place1=t1.place2 then t1.cnt + t2.cnt else 0 end as routeCount from test t1 join test t2  on t1.place1= t2.place2 and t2.place1=t1.place2").show() //
+
+
 
   }
 
@@ -51,5 +93,5 @@ object airLinePocData {
       false
     }
   }
-//test
+
 }
---
+++ src/main/scala/poc/utils/columnNames.scala
@@ -0,0 +1,22 @@
+package poc.utils
+
+
+object columnNames {
+  val AIRLINE_ID = "airlineId"
+  val AIRLINE_NAME = "airlineName"
+  val SOURCE_AIRPORT = "source_airport"
+  val SOURCE_AIRPORT_ID = "source_airport_id"
+  val DESTINATION_AIRPORT = "destination_airport"
+  val DESTINATION_AIRPORT_ID = "destination_airport_id"
+  val CODESHARE = "CodeShare"
+  val STOPS = "stops"
+  val UNKNOWN = "unknown"
+  // for airline
+  val ALIAS = "alias"
+  val IATA = "IATA"
+  val ICAO = "ICAO"
+  val CALLSIGN = "Callsign"
+  val COUNTRY = "country"
+  val ACTIVE = "active"
+
+}
